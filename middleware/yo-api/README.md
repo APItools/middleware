6ce0bba39dd532aec8a0c26492cdb967b15380e3
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-07-29 17:12:29 -0700

    middlware for Yo API

---
+++ middleware/yo-api/README.md
@@ -0,0 +1,12 @@
+# Yo API middlware
+
+## How to use it
+
+1. Define Yo API endpoint as you APItools service URL `http://api.justyo.com`
+2. Define the Yo URL callback with your APItools URL `http://token.APITOOLS.com`
+3. Add CORS-header middleware in the pipeline
+4. Change `YO_API_TOKEN` in `yo.lua` by your own Yo API key.
+5. Call `http://token.APITOOLS.com/yo` in your app to send individual Yos
+
+## Tutorial
+More descriptive tutorial can be found [here](https://docs.apitools.com/2014/07/15/develop-your-first-yo-app-with-apitools.html)
---
+++ middleware/yo-api/yo.lua
@@ -0,0 +1,28 @@
+return function(request, next_middleware)
+  local response
+  local apiToken = 'YO_API_TOKEN'
+  request.headers['Content-Type']='application/json'
+
+  -- endpoint to send an individual yo is called
+  if request.uri == '/yo/' then
+    local body = request.body
+
+    local yoUsername = split(body,'=')[2]
+    console.log(yoUsername)
+
+    request.body = '{"username":"'.. string.upper(yoUsername) .. '","api_token":"'..apiToken..'"}'
+
+  -- endpoint to Yo all is called
+    elseif request.uri =='/yoall/' then
+
+      request.body ='{"api_token":"'..apiToken..'"}'
+
+  -- callback url is called
+  elseif request.uri == '/' then
+    send.mail('me@email.com','New Yo subscriber', 'NEW Yo SUBSCRIBER '..request.args.username)
+    send.notification({msg="new subscriber " .. request.args.username, level='info'})
+  end
+    console.log(request.body)
+
+  return next_middleware()
+end
