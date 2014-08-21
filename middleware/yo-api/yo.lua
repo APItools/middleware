1d1079ec6aadb4892b4d902d91a9618629af2927
kikito <kikito@gmail.com>
2014-08-21 11:28:03 +0200

    fix luacheck messages in yo

--- middleware/yo-api/yo.lua
+++ middleware/yo-api/yo.lua
@@ -1,16 +1,15 @@
 -- split function to split a string by a delimiter
-function split(s, delimiter)
-    result = {}
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match)
-    end
-    return result
+local function split(s, delimiter)
+  local result = {}
+  for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+    table.insert(result, match)
+  end
+  return result
 end
 
 return function(request, next_middleware)
-  local response
   local apiToken = 'YO_API_TOKEN'
-  request.headers['Content-Type']='application/json'
+  request.headers['Content-Type'] = 'application/json'
 
   -- endpoint to send an individual yo is called
   if request.uri == '/yo/' then
@@ -19,19 +18,20 @@ return function(request, next_middleware)
     local yoUsername = split(body,'=')[2]
     console.log(yoUsername)
 
-    request.body = '{"username":"'.. string.upper(yoUsername) .. '","api_token":"'..apiToken..'"}'
+    request.body = '{"username":"'.. string.upper(yoUsername) .. '","api_token":"' .. apiToken .. '"}'
 
   -- endpoint to Yo all is called
     elseif request.uri =='/yoall/' then
 
-      request.body ='{"api_token":"'..apiToken..'"}'
+      request.body = '{"api_token":"' .. apiToken .. '"}'
 
   -- callback url is called
   elseif request.uri == '/' then
-    send.mail('me@email.com','New Yo subscriber', 'NEW Yo SUBSCRIBER '..request.args.username)
+    send.mail('me@email.com','New Yo subscriber', 'NEW Yo SUBSCRIBER ' .. request.args.username)
     send.notification({msg="new subscriber " .. request.args.username, level='info'})
   end
-    console.log(request.body)
+
+  console.log(request.body)
 
   return next_middleware()
 end
