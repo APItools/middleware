d2c9744773c82ac88c0991483960d53f92533156
kikito <kikito@gmail.com>
2014-08-22 18:56:12 +0200

    Allow environment reusing

--- spec/helper.lua
+++ spec/helper.lua
@@ -5,10 +5,10 @@ local sandbox = require 'spec.sandbox'
 
 local helper = {}
 
-helper.run = function(middleware, request, expected_response)
-  local environment     = env.new()
-  local sandboxed_mw    = sandbox.protect(middleware, {env = environment})
+helper.run = function(middleware, request, expected_response, environment)
+  environment = environment or env.new()
 
+  local sandboxed_mw    = sandbox.protect(middleware, {env = environment})
   local next_middleware = function() return expected_response end
   local response        = sandboxed_mw(request, next_middleware)
 
