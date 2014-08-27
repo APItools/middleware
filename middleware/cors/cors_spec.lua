917ee781afe37efd95ecdd878623757fd36d0ff2
kikito <kikito@gmail.com>
2014-08-27 13:41:57 +0200

    transform helper into expect-based

--- middleware/cors/cors_spec.lua
+++ middleware/cors/cors_spec.lua
@@ -5,39 +5,37 @@ local cors    = require 'cors.cors'
 describe("CORS", function()
   describe("when the status is not 404", function()
     it("does nothing", function()
-      local request  = { method = 'GET', uri_full = '/'}
+      local request  = { method = 'GET', uri = '/'}
       local response = { status = 200, body = 'ok' }
 
-      local res, env = helper.run(cors, request, response)
-
-      assert.same(res, response)
-      assert.same({}, env.bucket.middleware.get_keys())
-      assert.same({}, env.send.emails)
+      helper.expect(cors):called_with(request, response)
+        :to_pass(request)
+        :to_return(response)
+        :to_send_number_of_emails(0)
+        :to_set_number_of_keys_in_middleware_bucket(0)
     end)
   end)
 
   describe("when the status is 404", function()
     it("sends an email and marks the middleware bucket", function()
-      local request  = { method = 'GET', uri_full = '/'}
+      local request  = { method = 'GET', uri = '/'}
       local response = { status = 404, body = 'error' }
 
-      local res, env = helper.run(cors, request, response)
-
-      assert.same(res, response)
-      assert.is_truthy(env.bucket.middleware.values.last_mail)
-      assert.equal(#env.send.emails, 1)
-      assert.same(env.send.emails[1], {to='YOUR-MAIL-HERE@gmail.com', subject="A 404 has ocurred", message="a 404 error happened in /"})
+      helper.expect(cors):called_with(request, response)
+        :to_return(response)
+        :to_set_in_middleware_bucket('last_mail')
+        :to_send_email('YOUR-MAIL-HERE@gmail.com', 'A 404 has ocurred', 'a 404 error happened in http://localhost/')
     end)
 
     it("does not send two emails when called twice in rapid succession", function()
-      local request  = { method = 'GET', uri_full = '/'}
+      local request  = { method = 'GET', uri = '/'}
       local response = { status = 404, body = 'error' }
 
-      local res, env = helper.run(cors, request, response)
-      local res, env = helper.run(cors, request, response, env)
-
-      assert.same(res, response)
-      assert.equal(#env.send.emails, 1)
+      helper.expect(cors)
+        :called_with(request, response) -- not a typo, call the same request twice
+        :called_with(request, response)
+        :to_send_number_of_emails(1)
+        :to_set_number_of_keys_in_middleware_bucket(1)
     end)
 
   end)
--- spec/helper.lua
+++ spec/helper.lua
@@ -8,14 +8,129 @@ local url          = require 'socket.url' -- provided by luasocket
 
 local helper = {}
 
-helper.run = function(middleware, request, expected_response, environment)
-  environment = environment or env.new()
+local function copy_recursive(t)
+  if type(t) ~= 'table' then return t end
+  local c = {}
+  for k,v in pairs(t) do
+    c[copy_recursive(k)] = copy_recursive(v)
+  end
+  setmetatable(c, getmetatable(t))
+  return c
+end
 
-  local sandboxed_mw    = sandbox.protect(middleware, {env = environment})
-  local next_middleware = function() return expected_response end
-  local response        = sandboxed_mw(request, next_middleware)
+local function complete_request(req)
+  req = copy_recursive(req)
 
-  return response, environment
+  local parseable_uri = req.url or req.uri_full or req.uri_relative or req.uri
+  if parseable_uri then
+    local info = url.parse(parseable_uri)
+    req.scheme       = req.scheme       or info.scheme or 'http'
+    req.host         = req.host         or info.host or 'localhost'
+    req.uri          = (req.uri         or info.path):gsub('%?.*', '')
+    req.query        = req.query        or info.query or ''
+    req.args         = req.args         or querystring.parse(req.query)
+    local has_query  = req.query == '' and '' or '?'
+    req.uri_relative = req.uri_relative or (req.uri .. has_query .. req.query)
+    req.uri_full     = req.uri_full     or (req.scheme .. '://' .. req.host .. req.uri_relative)
+  else
+    error("Invalid request: Must provide at least one of the following: url, uri, uri_full or uri_relative")
+  end
+
+  req.method = req.method or 'GET'
+  req.headers = req.headers or {}
+  return req
+end
+
+local Expectation = {}
+local Expectation_mt = {__index = Expectation}
+
+function Expectation:called_with(request, backend_response)
+  backend_response = backend_response or {status = 200, body='ok'}
+  request          = complete_request(request)
+
+  self.request          = copy_recursive(request)
+  self.backend_response = copy_recursive(backend_response)
+  self.env              = self.env or env.new()
+
+  local next_middleware = function()
+    self.request_to_backend = copy_recursive(request)
+    return self.backend_response
+  end
+
+  local sandboxed_f   = sandbox.protect(self.middleware_f, {env = self.env})
+  self.response       = sandboxed_f(request, next_middleware)
+
+  return self
+end
+
+function Expectation:to_pass(expected_request_to_backend)
+  for k,v in pairs(expected_request_to_backend) do
+    if type(v) == 'table' then
+      assert.same(v, self.request_to_backend[k])
+    else
+      assert.equal(v, self.request_to_backend[k])
+    end
+  end
+
+  return self
+end
+
+function Expectation:to_receive(expected_backend_response)
+  for k,v in pairs(expected_backend_response) do
+    if type(v) == 'table' then
+      assert.same(v, self.backend_response[k])
+    else
+      assert.equal(v, self.backend_response[k])
+    end
+  end
+
+  return self
+end
+
+function Expectation:to_return(expected_returned_response)
+  for k,v in pairs(expected_returned_response) do
+    if type(v) == 'table' then
+      assert.same(v, self.response[k])
+    else
+      assert.equal(v, self.response[k])
+    end
+  end
+
+  return self
+end
+
+function Expectation:to_set_number_of_keys_in_middleware_bucket(n)
+  assert.same(#self.env.bucket.middleware.get_keys(), n)
+  return self
+end
+
+function Expectation:to_set_in_middleware_bucket(key, value)
+  if value == nil then
+    assert.not_nil(self.env.bucket.middleware.values[key])
+  else
+    assert.equal(self.env.bucket.middleware.values[key], value)
+  end
+  return self
+end
+
+function Expectation:to_send_number_of_emails(n)
+  assert.equal(#self.env.send.emails, n)
+  return self
+end
+
+function Expectation:to_send_email(to, subject, message)
+  local emails = self.env.send.emails
+  for i=1, #emails do
+    local email = emails[i]
+    if email.to == to and email.subject == subject and email.message == message then return self end
+  end
+  error(('The email to: %s with subject "%s" and message "%s" was not sent. Emails: %s'):format(
+    to, subject, message, inspect(emails)
+  ))
+end
+
+helper.expect = function(middleware_f)
+  return setmetatable({ middleware_f = middleware_f}, Expectation_mt)
 end
 
 return helper
