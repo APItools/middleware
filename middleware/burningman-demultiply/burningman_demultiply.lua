1faa50c9538291ed23adcc8b7f7d568321955584
kikito <kikito@gmail.com>
2014-09-02 18:29:02 +0200

    fix burningman-demultiply

--- middleware/burningman-demultiply/burningman_demultiply.lua
+++ middleware/burningman-demultiply/burningman_demultiply.lua
@@ -3,26 +3,26 @@ return function(request, next_middleware)
   local response = next_middleware()
   local events = json.decode(response.body)
   local newresponse ={}
-  
+
   for i=1,#events do
-    local e ={}
     local currentEvent = events[i];
-    e.title = currentEvent.title
-    e.desc = currentEvent.description
-    e.id = currentEvent.id
-    e.host = currentEvent.hosted_by_camp
-    e.url =currentEvent.url
-    e.location = currentEvent.other_location
-    e.category = currentEvent.event_type.abbr
-    
+
     for j=1,#currentEvent.occurrence_set do
-      e.start_time = currentEvent.occurrence_set[j].start_time
-      e.end_time = currentEvent.occurrence_set[j].end_time
-      table.insert(newresponse,e)
+      table.insert(newresponse,{
+        title       = currentEvent.title,
+        desc        = currentEvent.description,
+        id          = currentEvent.id,
+        host        = currentEvent.hosted_by_camp,
+        url         = currentEvent.url,
+        location    = currentEvent.other_location,
+        category    = currentEvent.event_type.abbr,
+        start_time  = currentEvent.occurrence_set[j].start_time,
+        end_time    = currentEvent.occurrence_set[j].end_time
+      })
     end
   end
-  
+
   response.body = json.encode(newresponse)
-  
-  return next_middleware()
+
+  return response
 end
