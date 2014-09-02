aaf80b4bad518569ad09aba50cf6f9669ae142fe
kikito <kikito@gmail.com>
2014-09-02 16:57:53 +0200

    Modify the way url, uri, uri_full & uri_relative work

--- spec/env/http_utils.lua
+++ spec/env/http_utils.lua
@@ -20,19 +20,21 @@ end
 function http_utils.complete_request(req)
   req = copy_recursive(req)
 
-  local parseable_uri = req.url or req.uri_full or req.uri_relative or req.uri
+  local parseable_uri = req.url or req.uri
   if parseable_uri then
     local info = url.parse(parseable_uri)
     req.scheme       = req.scheme       or info.scheme or 'http'
     req.host         = req.host         or info.host or 'localhost'
-    req.uri          = (req.uri         or info.path):gsub('%?.*', '')
-    req.query        = req.query        or info.query or ''
-    req.args         = req.args         or querystring.parse(req.query)
-    local has_query  = req.query == '' and '' or '?'
-    req.uri_relative = req.uri_relative or (req.uri .. has_query .. req.query)
-    req.uri_full     = req.uri_full     or (req.scheme .. '://' .. req.host .. req.uri_relative)
+    req.uri          = info.path:gsub('%?.*', '')
+    local query      = req.query        or info.query or ''
+    req.args         = req.args         or querystring.parse(query)
+    req.query        = req.query        or querystring.encode(req.args)
+    local has_query  = query == '' and '' or '?'
+    req.uri_relative = req.uri .. has_query .. req.query
+    req.uri_full     = req.scheme .. '://' .. req.host .. req.uri_relative
+    req.url          = req.uri_full
   else
-    error("Invalid request: Must provide at least one of the following: url, uri, uri_full or uri_relative")
+    error("Invalid request: Must provide at least a url or uri")
   end
 
   req.method  = req.method  or 'GET'
