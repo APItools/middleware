6a94714330ba54117d648f3011800c4a278bd4f4
kikito <kikito@gmail.com>
2014-09-03 15:01:33 +0200

    use advance_time in 404_alert_spec

--- middleware/404-alert/404_alert_spec.lua
+++ middleware/404-alert/404_alert_spec.lua
@@ -25,42 +25,72 @@ describe("404 alert", function()
   end)
 
   describe("when the status is 404", function()
-    it("sends an email and marks the middleware bucket", function()
-      local request         = spec.request({uri = '/'})
-      local next_middleware = spec.next_middleware(function()
-        assert.contains(request, {method = 'GET', uri = '/'})
-        return {status = 404, body = 'not ok'}
+    describe("when it happens once", function()
+      it("sends an email and marks the middleware bucket", function()
+        local request         = spec.request({uri = '/'})
+        local next_middleware = spec.next_middleware(function()
+          assert.contains(request, {method = 'GET', uri = '/'})
+          return {status = 404, body = 'not ok'}
+        end)
+
+        local response = alert(request, next_middleware)
+
+        assert.spy(next_middleware).was_called()
+        assert.contains(response, {status = 404, body = 'not ok'})
+
+        assert.truthy(spec.bucket.middleware.get('last_mail'))
+
+        assert.equal(#spec.sent.emails, 1)
+
+        local last_email = spec.sent.emails.last
+        assert.equal('YOUR-MAIL-HERE@gmail.com', last_email.to)
+        assert.equal('A 404 has ocurred', last_email.subject)
+        assert.equal('a 404 error happened in http://localhost/ see full trace: <trace_link>', last_email.message)
       end)
-
-      local response = alert(request, next_middleware)
-
-      assert.spy(next_middleware).was_called()
-      assert.contains(response, {status = 404, body = 'not ok'})
-
-      assert.truthy(spec.bucket.middleware.get('last_mail'))
-
-      assert.equal(#spec.sent.emails, 1)
-
-      local last_email = spec.sent.emails.last
-      assert.equal('YOUR-MAIL-HERE@gmail.com', last_email.to)
-      assert.equal('A 404 has ocurred', last_email.subject)
-      assert.equal('a 404 error happened in http://localhost/ see full trace: <trace_link>', last_email.message)
     end)
 
-    it("does not send two emails when called twice in rapid succession", function()
-      local request         = spec.request({method = 'GET', uri = '/'})
-      local next_middleware = spec.next_middleware(function()
-        assert.contains(request, {method = 'GET', uri = '/'})
-        return {status = 404, body = 'not ok'}
+    describe("when the 404 happens more than once", function()
+      describe("and the time between errors is smaller than the threshold", function()
+        it("only sends one email", function()
+          local request         = spec.request({method = 'GET', uri = '/'})
+          local next_middleware = spec.next_middleware(function()
+            assert.contains(request, {method = 'GET', uri = '/'})
+            return {status = 404, body = 'not ok'}
+          end)
+
+          alert(request, next_middleware)
+          spec.advance_time(10)
+          alert(request, next_middleware) -- twice
+          assert.spy(next_middleware).was_called(2)
+
+
+          assert.equal(1, #spec.sent.emails)
+        end)
+
+        describe("and the time between errors is greater than the threshold", function()
+          it("sends several emails", function()
+            local request         = spec.request({method = 'GET', uri = '/'})
+            local next_middleware = spec.next_middleware(function()
+              assert.contains(request, {method = 'GET', uri = '/'})
+              return {status = 404, body = 'not ok'}
+            end)
+
+            alert(request, next_middleware)
+            spec.advance_time(5 * 60 + 1)
+            alert(request, next_middleware)
+            assert.spy(next_middleware).was_called(2)
+
+
+            assert.equal(2, #spec.sent.emails)
+          end)
+        end)
       end)
 
-      alert(request, next_middleware)
-      alert(request, next_middleware) -- twice
-      assert.spy(next_middleware).was_called(2)
 
-
-      assert.equal(1, #spec.sent.emails)
     end)
+
+
+
 
   end)
 end)
