44a28db7817d0abfa7144b926454dde3fba8e1f6
vramosp <vanessa@3scale.net>
2014-12-03 11:02:30 -0800

    fixed double //

--- middleware/send-res-size-keen/send_res_size_keen.lua
+++ middleware/send-res-size-keen/send_res_size_keen.lua
@@ -2,6 +2,6 @@ return function (request, next_middleware)
   local response = next_middleware()
   local api_key = "YOUR-KEEN-IO-API-KEY-HERE"
   local size = base64.encode(json.encode({ size = #response.body }))
-  http.get('https://api.keen.io/3.0/projects//<PROJECT_ID>/events/<EVENT_COLLECTION>?api_key=' .. api_key .. '&data=' .. size)
+  http.get('https://api.keen.io/3.0/projects/<PROJECT_ID>/events/<EVENT_COLLECTION>?api_key=' .. api_key .. '&data=' .. size)
   return response
 end
