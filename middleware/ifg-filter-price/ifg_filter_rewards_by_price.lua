307a5be72293c51146b66cadf39c30b3a6e7cb63
kikito <kikito@gmail.com>
2015-05-25 16:28:41 +0200

    use actual prices instead of undefined variables

--- middleware/ifg-filter-price/ifg_filter_rewards_by_price.lua
+++ middleware/ifg-filter-price/ifg_filter_rewards_by_price.lua
@@ -3,8 +3,10 @@ return function(request, next_middleware)
 
   local data = json.decode(response.body).data
 
-  local max_price = MAXIMUM_PRICE
-  local min_price = MINIMUM_PRICE
+  -- CHANGE THIS TO WHATEVER YOU WANT
+  local max_price = 9
+  local min_price = 2
+
   local r={}
   for i=1,#data do
     if(tonumber(data[i].face_value)>= min_price and tonumber(data[i].face_value) <= max_price) then
