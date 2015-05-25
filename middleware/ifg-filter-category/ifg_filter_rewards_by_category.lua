a4fc2f168d7ca4eaee164466e0cb799abde45d4f
kikito <kikito@gmail.com>
2015-05-25 17:41:44 +0200

    localize indexOf

--- middleware/ifg-filter-category/ifg_filter_rewards_by_category.lua
+++ middleware/ifg-filter-category/ifg_filter_rewards_by_category.lua
@@ -1,4 +1,4 @@
-indexOf = function( t, object )
+local indexOf = function( t, object )
   if "table" == type( t ) then
     for i = 1, #t do
         if object == t[i] then
@@ -7,7 +7,7 @@ indexOf = function( t, object )
     end
     return -1
   else
-    error("table.indexOf expects table for first argument, " .. type(t) .. " given")
+    error("indexOf expects table for first argument, " .. type(t) .. " given")
   end
 end
 
